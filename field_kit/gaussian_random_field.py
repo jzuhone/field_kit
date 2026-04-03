e4529f09328c81f1698a6387a95c3eb4f9af42b9
John ZuHone <jzuhone@gmail.com>
2026-04-03 14:57:52 -0400

    bug fixes and improvements for GRF

--- field_kit/gaussian_random_field.py
+++ field_kit/gaussian_random_field.py
@@ -5,7 +5,7 @@
 import numpy as np
 from numba import njit, prange
 from scipy.fft import fftfreq
-from .fourier_analysis import FFTArray
+from .fourier_analysis import FFTArray, FourierAnalysis
 
 sqrt2 = 2.0**0.5
 
@@ -124,8 +124,10 @@ class GaussianRandomField:
         self.delta = self.width / self.ddims
         self.ndim = self.left_edge.size
         self.x = np.linspace(self.left_edge[0]+0.5*self.delta[0], self.right_edge[0]-0.5*self.delta[0], self.ddims[0])
-        self.y = np.linspace(self.left_edge[1]+0.5*self.delta[1], self.right_edge[1]-0.5*self.delta[1], self.ddims[1])
-        self.z = np.linspace(self.left_edge[2]+0.5*self.delta[2], self.right_edge[2]-0.5*self.delta[2], self.ddims[2])
+        if self.ndim > 1:
+            self.y = np.linspace(self.left_edge[1]+0.5*self.delta[1], self.right_edge[1]-0.5*self.delta[1], self.ddims[1])
+        if self.ndim == 3:
+            self.z = np.linspace(self.left_edge[2]+0.5*self.delta[2], self.right_edge[2]-0.5*self.delta[2], self.ddims[2])
 
         self.kx = 2.0*np.pi*fftfreq(self.ddims[0], d=self.delta[0])
         if self.ndim > 1:
@@ -147,11 +149,7 @@ class GaussianRandomField:
         sigma /= np.sqrt(np.prod(self.width))
         self.sigma = sigma
 
-    def generate_scalar_field_realization(self, fourier_space=False):
-        """
-        Generate a random realization of the field as a scalar field.
-        """
-
+    def _generate_field_realization(self):
         if self.sigma is None:
             self._compute_sigma()
 
@@ -168,22 +166,42 @@ class GaussianRandomField:
         else:
             v = enforce_hermitian_symmetry_3d(v)
 
+        return v
+
+    def generate_scalar_field_realization(self, fourier_space=False):
+        """
+        Generate a random realization of the field as a scalar field.
+        """
+        v = self._generate_field_realization()
+
         if fourier_space:
             return FFTArray(v)
         else:
             v = np.fft.ifftn(v, norm="forward")
             return v.real
 
-    def generate_vector_field_realization(self, fourier_space=False):
+    def generate_vector_field_realization(self, fourier_space=False, divergence_free=False):
         """
         Generate a random realization of the field as a vector field.
         """
+        if self.ndim == 1:
+            raise NotImplementedError("Vector fields are not implemented for 1D.")
 
         field = []
         for i in range(self.ndim):
-            field.append(self.generate_scalar_field_realization(fourier_space=fourier_space))
+            field.append(self._generate_field_realization())
         field = np.asarray(field)
+
+        if divergence_free:
+            fa = FourierAnalysis(self.width, self.ddims)
+            k, kmag = fa.generate_waves("central")
+            with np.errstate(divide="ignore", invalid="ignore"):
+                field -= k * np.sum(k * field, axis=0) / (kmag * kmag)
+            np.nan_to_num(field, copy=False)
+            field *= {2: 2.0**0.5, 3: 1.5**0.5}[self.ndim] # renormalize to preserve power spectrum amplitude
+
         if fourier_space:
             return FFTArray(field)
         else:
-            return field
+            field = np.fft.ifftn(field, axes=tuple(range(1, self.ndim+1)), norm="forward")
+            return field.real
